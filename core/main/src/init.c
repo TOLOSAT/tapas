ef65ce6cf186698df4833c8a21c9806d60be14b6
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-04-30 17:28:13 +0000

    Adding IIC functions

--- core/main/src/init.c
+++ core/main/src/init.c
@@ -27,19 +27,19 @@
 
 /************************** Function Prototypes ******************************/
 
-static void I2C1_Init(void);
-
 void EnableUserButtonIt(void);
 extern void Error_Handler(void);
 extern void InitMonitorHandler(uartInst_t *uart_inst);
 
 /************************** Variable Definitions *****************************/
 
-I2C_HandleTypeDef hi2c1;
-
+iicInst_t iic1_inst ={
+    .iic_ref = I2C1,
+    .drive_type = IIC_POLLING_MASTER_DRIVE,
+};
 uartInst_t uart2_inst = {
-    .drive_type = UART_POLLING_DRIVE,
     .uart_ref = USART2,
+    .drive_type = UART_POLLING_DRIVE,
     .baud_rate = 115200,
 };
 gpioInst_t led2_inst = {
@@ -77,7 +77,7 @@ uint32_t init(void)
     UartOpen(&uart2_inst);
 
     // I2Cs Initialisation
-    I2C1_Init();
+    IicOpen(&iic1_inst);
     
     // Monitor Initialisation
     InitMonitorHandler(&uart2_inst);
@@ -98,26 +98,4 @@ void EnableUserButtonIt(void)
     /* EXTI interrupt init*/
     HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-}
-
-/**
- * @brief I2C1 Initialization Function
- * @param None
- * @retval None
- */
-static void I2C1_Init(void)
-{
-    hi2c1.Instance = I2C1;
-    hi2c1.Init.ClockSpeed = 100000;
-    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-    hi2c1.Init.OwnAddress1 = 0;
-    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    hi2c1.Init.OwnAddress2 = 0;
-    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-    if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-    {
-        Error_Handler();
-    }
 }
