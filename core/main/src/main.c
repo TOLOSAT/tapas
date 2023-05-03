db047cee9eebdff2b4b4d39a89a1bc5d41aeefa3
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-05-03 19:03:37 +0000

    Some cleanup in hal functions because it was spaghetti code

--- core/main/src/main.c
+++ core/main/src/main.c
@@ -68,7 +68,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
     if (GPIO_Pin == USER_BUTTON_PIN) // If The INT Source Is EXTI Line9 (A9 Pin)
     {
-        printf("Hello from interrupt !\n");
+        printf("\nHello from interrupt !\n\n");
     }
 }
 
