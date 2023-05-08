2a2c959dd58283120b80bfb174426e1e63fc5ba6
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-05-08 12:05:15 +0000

    Few changes

--- core/main/inc/errors_mgmt.h
+++ core/main/inc/errors_mgmt.h
@@ -29,7 +29,8 @@
  * @typedef errorsSanction_t
  * @brief   Type of sanctions enum definition
  */
-typedef enum {
+typedef enum
+{
     NO_SANCTION     = 0u,    /**< No sanction needed */
     ERROR_HANDLER   = 1u,    /**< Error has to be dealed in the error handler */
     RESET_HANDLER   = 2u,    /**< Error needs a reset of the system */
