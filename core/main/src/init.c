59f356df2c0c1d7fcde1f4cf0f0a1bcec93c74b9
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-05-27 17:40:24 +0200

    Add modifications in order to reduce #if defined()

--- core/main/src/init.c
+++ core/main/src/init.c
@@ -37,23 +37,23 @@ extern void InitMonitorHandler(uartInst_t *uart_inst);
 /************************** Variable Definitions *****************************/
 
 /**
- * @var     TOLOSAT_UART_TMTC_DMA_RX
+ * @var     TAPAS_UART_TMTC_DMA_RX
  * @brief   DMA USART1 RX instance declaration
  */
-DMA_HandleTypeDef TOLOSAT_UART_TMTC_DMA_RX;
+DMA_HandleTypeDef TAPAS_UART_TMTC_DMA_RX;
 
 /**
- * @var     TOLOSAT_UART_TMTC_DMA_TX
+ * @var     TAPAS_UART_TMTC_DMA_TX
  * @brief   DMA USART1 TX instance declaration
  */
-DMA_HandleTypeDef TOLOSAT_UART_TMTC_DMA_TX;
+DMA_HandleTypeDef TAPAS_UART_TMTC_DMA_TX;
 
 /**
  * @var     iic_avionic_inst
  * @brief   I2C avionic instance declaration
  */
 iicInst_t iic_avionic_inst = {
-    .iic_ref = TOLOSAT_I2C_AVIONIC,
+    .iic_ref = TAPAS_I2C_AVIONIC,
     .drive_type = IIC_IT_MASTER_DRIVE,
 };
 
@@ -62,7 +62,7 @@ iicInst_t iic_avionic_inst = {
  * @brief   UART tmtc instance declaration
  */
 uartInst_t uart_tmtc_inst = {
-    .uart_ref = TOLOSAT_UART_TMTC,
+    .uart_ref = TAPAS_UART_TMTC,
     .drive_type = UART_DMA_DRIVE,
     .baud_rate = 115200,
 };
@@ -72,7 +72,7 @@ uartInst_t uart_tmtc_inst = {
  * @brief   UART print instance declaration
  */
 uartInst_t uart_print_inst = {
-    .uart_ref = TOLOSAT_UART_PRINT,
+    .uart_ref = TAPAS_UART_PRINT,
     .drive_type = UART_POLLING_DRIVE,
     .baud_rate = 115200,
 };
@@ -82,7 +82,7 @@ uartInst_t uart_print_inst = {
  * @brief   UART payload instance declaration
  */
 uartInst_t uart_pl_inst = {
-    .uart_ref = TOLOSAT_UART_PL,
+    .uart_ref = TAPAS_UART_PL,
     .drive_type = UART_INTERRUPT_DRIVE,
     .baud_rate = 115200,
 };
