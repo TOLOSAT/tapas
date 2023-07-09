418b6afacdaa92fe95e113eb404685baba021167
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-09 09:21:30 +0000

    Update doxygen

--- core/main/src/main.c
+++ core/main/src/main.c
@@ -57,8 +57,8 @@ int main(void)
 }
 
 /**
- * @brief  EXTI line detection callback.
- * @param  GPIO_Pin Specifies the port pin connected to corresponding EXTI line.
+ * @brief       EXTI line detection callback.
+ * @param[in]   GPIO_Pin Specifies the port pin connected to corresponding EXTI line.
  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
