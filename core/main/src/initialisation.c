19540c1f16f4df1e51e1a71613f9d6fdbc962499
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-20 13:35:02 +0000

    Disk mount is now in the open function & other improvements

--- core/main/src/initialisation.c
+++ core/main/src/initialisation.c
@@ -70,7 +70,7 @@ void init(void)
 
     // File System Initialisation
     status = FsOpen(&fs_inst);
-    CheckErrors(status, FDIR_ERROR_HANDLER);
+    CheckErrors(status, FDIR_NO_SANCTION);
     
     // Monitor Initialisation
     status = InitMonitoring();
