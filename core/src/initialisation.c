0aa23096058de04faac80893987b98816845ec05
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-08-21 21:45:32 +0200

    Now peripherals are initialised through a desc table

--- core/src/initialisation.c
+++ core/src/initialisation.c
@@ -49,8 +49,8 @@ void IN_CORE_TEXT_SECTION init(void)
     status = BSPLateInit();
     CheckErrors(status, FDIR_ERROR_HANDLER);
 
-    // Platform Initialisation
-    status = PlatformInit();
+    // Peripherals Initialisation
+    status = InitPeripherals();
     CheckErrors(status, FDIR_ERROR_HANDLER);
 
     // RTC Initialisation
