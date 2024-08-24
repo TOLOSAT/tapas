7173b6256079178246cde886ab4ee70074dcf369
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-08-24 18:05:30 +0200

    Added mutexes to peripherals

--- core/src/console.c
+++ core/src/console.c
@@ -55,7 +55,7 @@ static void ConsoleSync(void);
 /*************************** Variables Definitions ***************************/
 
 #if defined(CONSOLE_MODE_UART)
-extern uartInst_t uart_print_inst;
+extern uartInst_t g_uart_print_inst;
 #endif
 
 #if defined(CONSOLE_MODE_CIRCULAR_BUFFER)
@@ -395,7 +395,7 @@ static void IN_CORE_TEXT_SECTION ConsolePrintChar(char c)
 {
 #if defined(CONSOLE_MODE_UART)
     // Function Core
-    (void)UartWrite(&uart_print_inst, (uartMsg_t *)&c, sizeof(char));
+    (void)UartWrite(&g_uart_print_inst, (uartMsg_t *)&c, sizeof(char));
 #elif defined(CONSOLE_MODE_FILE)
     // Variable declaration
     fsSize_t console_size = 0u;
