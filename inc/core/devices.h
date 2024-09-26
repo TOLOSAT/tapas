2a6a2c2c0e513aaee6e132cb33cc1c458981f027
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-09-26 19:50:44 +0200

    Now Device API is used instead of FS API in the user space

--- inc/core/devices.h
+++ inc/core/devices.h
@@ -29,7 +29,9 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define MAX_NUMBER_DEVICES  64u /**< Maximum number of devices that can be allocated */
+#define MAX_NUMBER_DEVICES      64u /**< Maximum number of devices that can be allocated */
+
+#define DEVICE_NO_EXTRA_DATA    0u      /**< Constant to inform there is no extra data */
 
 #define IOCTL_LOCK_DEVICE       (-1u)    /**< Generic IOTCL command to lock the device linked file or peripheral */
 #define IOCTL_UNLOCK_DEVICE     (-2u)    /**< Generic IOTCL command to unlock the device linked file or peripheral */
