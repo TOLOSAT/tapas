06269e20f1e46ffafa14d3d469a4cac30ea22d0f
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-10-02 13:25:33 +0200

    Updated linkerscript with bss section and removed all IN_{}_SECTION defines in the code

---
+++ common.ld.h
@@ -0,0 +1,58 @@
+/* BSP contained sections */
+#define BSP_TEXT_SEGMENT    . = ALIGN(4); \
+                            _bsp_text_start_ = .; \
+                            *libbsp*.a:*(.text .text.*) \
+                            . = ALIGN(4); \
+                            _bsp_text_end_ = .;
+
+#define BSP_DATA_SEGMENT    . = ALIGN(4); \
+                            _bsp_data_start_ = .; \
+                            *libbsp*.a:*(.data .data.*) \
+                            . = ALIGN(4); \
+                            _bsp_data_end_ = .;
+
+#define BSP_BSS_SEGMENT    . = ALIGN(4); \
+                            _bsp_bss_start_ = .; \
+                            *libbsp*.a:*(.bss .bss.*) \
+                            . = ALIGN(4); \
+                            _bsp_bss_end_ = .;
+
+/* HAL contained sections */
+
+#define HAL_TEXT_SEGMENT    . = ALIGN(4); \
+                            _hal_text_start_ = .; \
+                            *libhal*.a:*(.text .text.*) \
+                            . = ALIGN(4); \
+                            _hal_text_end_ = .;
+
+#define HAL_DATA_SEGMENT    . = ALIGN(4); \
+                            _hal_data_start_ = .; \
+                            *libhal*.a:*(.data .data.*) \
+                            . = ALIGN(4); \
+                            _hal_data_end_ = .;
+
+#define HAL_BSS_SEGMENT    . = ALIGN(4); \
+                            _hal_bss_start_ = .; \
+                            *libhal*.a:*(.bss .bss.*) \
+                            . = ALIGN(4); \
+                            _hal_bss_end_ = .;
+
+/* FATFS contained sections */
+
+#define FATFS_TEXT_SEGMENT  . = ALIGN(4); \
+                            _fatfs_text_start_ = .; \
+                            *libfatfs*.a:*(.text .text.*) \
+                            . = ALIGN(4); \
+                            _fatfs_text_end_ = .;
+
+#define FATFS_DATA_SEGMENT    . = ALIGN(4); \
+                            _fatfs_data_start_ = .; \
+                            *libfatfs*.a:*(.data .data.*) \
+                            . = ALIGN(4); \
+                            _fatfs_data_end_ = .;
+
+#define FATFS_BSS_SEGMENT    . = ALIGN(4); \
+                            _fatfs_bss_start_ = .; \
+                            *libfatfs*.a:*(.bss .bss.*) \
+                            . = ALIGN(4); \
+                            _fatfs_bss_end_ = .;
--- kernel.ld.h
+++ kernel.ld.h
@@ -1,14 +1,21 @@
 /* Kernel contained sections */
 #define KERNEL_TEXT_SEGMENT     . = ALIGN(4); \
-                                _text_kernel_start_ = .; \
+                                _kernel_text_start_ = .; \
                                 *libkernel*.a:*(.text .text.*) \
                                 *libos*.a:*(.text .text.*) \
                                 . = ALIGN(4); \
-                                _text_kernel_end_ = .;
+                                _kernel_text_end_ = .;
 
 #define KERNEL_DATA_SEGMENT     . = ALIGN(4); \
-                                _data_kernel_start_ = .; \
-                                *libkernel*.a:*(.data .data.* .bss .bss.*) \
-                                *libos*.a:*(.data .data.* .bss .bss.*) \
+                                _kernel_data_start_ = .; \
+                                *libkernel*.a:*(.data .data.*) \
+                                *libos*.a:*(.data .data.*) \
                                 . = ALIGN(4); \
-                                _data_kernel_end_ = .;
+                                _kernel_data_end_ = .;
+
+#define KERNEL_BSS_SEGMENT      . = ALIGN(4); \
+                                _kernel_bss_start_ = .; \
+                                *libkernel*.a:*(.bss .bss.*) \
+                                *libos*.a:*(.bss .bss.*) \
+                                . = ALIGN(4); \
+                                _kernel_bss_end_ = .;
