810d8349ffe3f3db93f90e13b5c31aaebbbd86c1
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-10-12 13:39:31 +0200

    Moved linkers into a specific folder, and moved Reset_Handler, Default_Handler and System function and data to the kernel sections

--- kernel.ld.h
+++ kernel.ld.h
@@ -1,13 +1,19 @@
 /* Kernel contained sections */
 #define KERNEL_TEXT_SEGMENT     . = ALIGN(4); \
                                 _kernel_text_start_ = .; \
+                                *(.text.Reset_Handler) \
+                                *(.text.SystemInit) \
+                                *(.text.SystemClock_Config) \
                                 *libkernel*.a:*(.text .text.*) \
                                 *libos*.a:*(.text .text.*) \
+                                *(.text.Default_Handler) \
                                 . = ALIGN(4); \
                                 _kernel_text_end_ = .;
 
 #define KERNEL_DATA_SEGMENT     . = ALIGN(4); \
                                 _kernel_data_start_ = .; \
+                                *(.data.SystemCoreClock) \
+                                *(.data.SystemD2Clock) \
                                 *libkernel*.a:*(.data .data.*) \
                                 *libos*.a:*(.data .data.*) \
                                 . = ALIGN(4); \
