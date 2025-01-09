c07083c08cfc8e055019dff8c393425f1d3cd4cf
Matteo Planchet <matteo.planchet@gmail.com>
2025-01-09 20:16:07 +0100

    Fix linter rules

--- src/drv/ST-wrapper/drv_wdg.c
+++ src/drv/ST-wrapper/drv_wdg.c
@@ -13,13 +13,13 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define TIMEOUT_MS 500u /**< Timeout of the watchdog, in milliseconds >*/
-#define T_LSI (1u/32000u) /**< Time of the LSI clock, in seconds >*/
+#define TIMEOUT_MS 500u /**< Timeout of the watchdog, in milliseconds*/
+#define T_LSI (1u/32000u) /**< Time of the LSI clock, in seconds*/
 
 /**
  * @def     MS_TO_WDG_COUNTER_VALUE(timeout_ms)
  * @brief   Calculates the IWDG counter value for a given timeout in ms
- * @see <a href="https://www.st.com/resource/en/product_training/STM32WB-WDG_TIMERS-Independent-Watchdog-IWDG.pdf#page=10">Reference</a>
+ * @see     STM32WB-IWDG Revision 1.0 
  * IWDG_PRESCALER_4 = 4 * 2^0, so PR = 0
  */
 #define MS_TO_WDG_COUNTER_VALUE(timeout_ms) (((timeout_ms) / (T_LSI * IWDG_PRESCALER_4)) - 1u)
