c52687fa84ad8ee02d487fa2b2ef63ad08fcbdf4
Matteo Planchet <matteo.planchet@gmail.com>
2025-01-09 19:39:39 +0100

    Add docs + compute watchdog counter value macro

--- src/drv/ST-wrapper/drv_wdg.c
+++ src/drv/ST-wrapper/drv_wdg.c
@@ -1,5 +1,5 @@
 /**
- * @file    drv_WDG.c
+ * @file    drv_wdg.c
  * @author  Matteo Planchet
  * @author  Mathis Steinberger
  * @brief   Source file for WDG functions
@@ -13,8 +13,16 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define WDG_DOWN_COUNTER_VALUE 3999u   /**< Down counter value for 0.5s */
-// TODO: Make a formula macro with the timeout as input
+#define TIMEOUT_MS 500 /**< Timeout of the watchdog, in milliseconds >*/
+#define T_LSI 1/32000 /**< Time of the LSI clock, in seconds >*/
+
+/**
+ * @def     MS_TO_WDG_COUNTER_VALUE(timeout_ms)
+ * @brief   Calculates the IWDG counter value for a given timeout in ms
+ * @see <a href="https://www.st.com/resource/en/product_training/STM32WB-WDG_TIMERS-Independent-Watchdog-IWDG.pdf#page=10">Reference</a>
+ * IWDG_PRESCALER_4 = 4 * 2^0, so PR = 0
+ */
+#define MS_TO_WDG_COUNTER_VALUE(timeout_ms) ((timeout_ms / (T_LSI * IWDG_PRESCALER_4)) - 1)
 
 /*************************** Functions Declarations **************************/
 
@@ -24,6 +32,12 @@ static IWDG_HandleTypeDef wdg_inst = {0};
 
 /*************************** Functions Definitions ***************************/
 
+/**
+ * @fn    InitWatchDog()
+ * @brief  Initialises the watchdog 
+ *
+ * @return returnCode_t 
+ */
 returnCode_t InitWatchDog(void) 
 {
     // Variable Initialisation
@@ -33,7 +47,7 @@ returnCode_t InitWatchDog(void)
     // Function Core
     wdg_inst.Instance = IWDG;
     wdg_inst.Init.Prescaler = IWDG_PRESCALER_4;
-    wdg_inst.Init.Reload = WDG_DOWN_COUNTER_VALUE;
+    wdg_inst.Init.Reload = MS_TO_WDG_COUNTER_VALUE(TIMEOUT_MS);
     test_val = HAL_IWDG_Init(&wdg_inst);
     if (test_val != HAL_OK) {
         return_value = RET_ERROR;
@@ -41,6 +55,13 @@ returnCode_t InitWatchDog(void)
 
     return return_value;
 }
+
+/**
+ * @fn    PetWatchDog()
+ * @brief  Reloads the watchdog
+ * 
+ * @return Nothing
+ */
 void PetWatchDog(void)
 {
     __HAL_IWDG_RELOAD_COUNTER(&wdg_inst);
