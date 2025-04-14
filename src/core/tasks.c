71d398a49aaf98a89dc81e79101a682a504da176
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-04-14 20:36:18 +0200

    fix: naming

--- src/core/tasks.c
+++ src/core/tasks.c
@@ -43,7 +43,7 @@ void CreateTasks(void)
                 xTaskCreateStatic(g_tasks_conf_table[TASKNO_TO_LINENO(task)].function,                         // Task function
                                   g_tasks_conf_table[TASKNO_TO_LINENO(task)].name,                             // Task main
                                   g_tasks_conf_table[TASKNO_TO_LINENO(task)].stack_size / sizeof(StackType_t), // Task stack size
-                                  NULL,                                                                  // Task function parameter
+                                  NULL,                                                                        // Task function parameter
                                   g_tasks_conf_table[TASKNO_TO_LINENO(task)].priority,                         // Task priority
                                   g_tasks_conf_table[TASKNO_TO_LINENO(task)].p_stack,                          // Task stack
                                   g_tasks_conf_table[TASKNO_TO_LINENO(task)].p_tcb);                           // Task TCB
