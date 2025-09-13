9b0fdcb59aecb816c8b95259da4492d53646f5c9
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-09-13 14:37:43 +0200

    ref: Moved FreeRTOS out of the TCMs

--- kernel.ld.h
+++ kernel.ld.h
@@ -6,14 +6,12 @@
                                 *(.text.SystemInit) \
                                 *(.text.SystemClock_Config) \
                                 *libkernel*.a:*(.text .text.*) \
-                                *libos*.a:*(.text .text.*) \
                                 . = ALIGN(4); \
                                 __kernel_text_end__ = .;
 
 #define KERNEL_RODATA_SEGMENT   . = ALIGN(4); \
                                 __kernel_rodata_start__ = .; \
                                 *libkernel*.a:*(.rodata .rodata.*) \
-                                *libos*.a:*(.rodata .rodata.*) \
                                 *(.conf_tables) \
                                 . = ALIGN(4); \
                                 __kernel_rodata_end__ = .;
@@ -23,7 +21,6 @@
                                 *(.data.SystemCoreClock) \
                                 *(.data.SystemD2Clock) \
                                 *libkernel*.a:*(.data .data.*) \
-                                *libos*.a:*(.data .data.*) \
                                 *(.desc_tables) \
                                 *(.task_tcbs) \
                                 *(.buffer_queues) \
@@ -36,6 +33,5 @@
 #define KERNEL_BSS_SEGMENT      . = ALIGN(4); \
                                 __kernel_bss_start__ = .; \
                                 *libkernel*.a:*(.bss .bss.*) \
-                                *libos*.a:*(.bss .bss.*) \
                                 . = ALIGN(4); \
                                 __kernel_bss_end__ = .;
