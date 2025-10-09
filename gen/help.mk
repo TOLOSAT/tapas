c6b81bfc8b25ce5878ae26ef9ff67e4d3d089eb3
TheoBessel <theo.bessel@outlook.com>
2025-10-09 19:57:20 +0200

    style: Updated make config messages

--- gen/help.mk
+++ gen/help.mk
@@ -1,5 +1,19 @@
 # Makefile defining the help message
 
+# Colors for output
+GREEN := \033[32m
+YELLOW := \033[33m
+RED := \033[31m
+BLUE := \033[34m
+
+# Fonts settings
+BOLD := \033[1m
+NORMAL := \033[0m
+ITALIC := \033[3m
+UNDERLINE := \033[4m
+
+RESET := \033[0m # No Color
+
 help:
 	@echo "Usage: make <target>"
 	@echo ""
