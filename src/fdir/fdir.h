4bc9fe1b9d165ca70141da42560fa01979606c7c
Athos-0day <athos.0day@protonmail.com>
2026-03-05 19:09:13 +0100

    fix: comment format

--- src/fdir/fdir.h
+++ src/fdir/fdir.h
@@ -34,11 +34,12 @@
  * @brief System-wide severity levels for FDIR and event reporting according to ECSS-E-ST-70-41C.
  * @note These levels are mapped to PUS5 event severities.
  */
-typedef enum {
-    SEVERITY_INFORMATIVE = 0u, /**< Informative event.       */
-    SEVERITY_LOW         = 1u, /**< Low severity.          */
-    SEVERITY_MEDIUM      = 2u, /**< Medium severity.   */
-    SEVERITY_HIGH        = 3u, /**< High severity.  */
+typedef enum 
+{
+    SEVERITY_INFORMATIVE = 0u,  /**< Informative event.                   */
+    SEVERITY_LOW         = 1u,  /**< Low severity.                        */
+    SEVERITY_MEDIUM      = 2u,  /**< Medium severity.                     */
+    SEVERITY_HIGH        = 3u,  /**< High severity.                       */
 } severityLevel_t;
 
 /**
