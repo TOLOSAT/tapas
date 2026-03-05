ba10929d4a441d6aeee89a22576013226730e26b
Athos-0day <athos.0day@protonmail.com>
2026-03-05 18:34:17 +0100

    chore: comment modif

--- src/fdir/fdir.h
+++ src/fdir/fdir.h
@@ -31,8 +31,8 @@
 /***************************** Types Definitions *****************************/
 
 /**
- * @brief System-wide severity levels for FDIR and event reporting.
- * @note These levels are mapped to PUS5 (Service 5) event severities.
+ * @brief System-wide severity levels for FDIR and event reporting accordinf to ECSS-E-ST-70-41C.
+ * @note These levels are mapped to PUS5 event severities.
  */
 typedef enum {
     SEVERITY_INFORMATIVE = 0u, /**< Informative event (No action).       */
