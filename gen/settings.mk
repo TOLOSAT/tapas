dfac2e5ca9336fdce705614e618225c397beaadb
Merlin Kooshmanian <mkooshmanian@gmail.com>
2026-05-31 12:32:13 +0200

    fix: makefile duplication

--- gen/settings.mk
+++ gen/settings.mk
@@ -89,23 +89,6 @@ else
 LOAD_MEMORY = flash
 endif
 
-
-# Detect builds launched with GNU make parallel jobs.
-PARALLEL_BUILD = $(strip \
-	$(findstring --jobserver-auth,$(MAKEFLAGS)) \
-	$(findstring --jobserver-fds,$(MAKEFLAGS)) \
-	$(filter -j,$(MAKEFLAGS)) \
-	$(filter -j%,$(MAKEFLAGS)))
-QUIET_RECIPE = @true
-
-# Detect builds launched with GNU make parallel jobs.
-PARALLEL_BUILD = $(strip \
-	$(findstring --jobserver-auth,$(MAKEFLAGS)) \
-	$(findstring --jobserver-fds,$(MAKEFLAGS)) \
-	$(filter -j,$(MAKEFLAGS)) \
-	$(filter -j%,$(MAKEFLAGS)))
-QUIET_RECIPE = @true
-
 # Detect builds launched with GNU make parallel jobs.
 PARALLEL_BUILD = $(strip \
 	$(findstring --jobserver-auth,$(MAKEFLAGS)) \
