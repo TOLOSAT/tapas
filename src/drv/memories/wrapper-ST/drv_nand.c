bb9ed4c95608b09343c65c69e74db65ad643e9c4
Merlin Kooshmanian <mkooshmanian@gmail.com>
2026-09-13 18:42:23 +0200

    feat/drv_nand: add internal ECC status handling to page reads

--- src/drv/memories/wrapper-ST/drv_nand.c
+++ src/drv/memories/wrapper-ST/drv_nand.c
@@ -19,13 +19,20 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define NAND_TIMEOUT               1000u            /**< NAND Timeout for ST HAL */
-#define NAND_MAX_BLOCK_SIZE_BYTES  262144u          /**< NAND block maximum size used for write buffering (correspond to 64 pages of 4096 bytes) */
-#define NAND_INVALID_BLOCK_NUMBER  ((uint32_t)-1)   /**< NAND Invalid block id (used to default the value of nand_write_buffer_block) */
-#define NAND_CMD_SET_FEATURE       ((uint8_t)0xEFu) /**< NAND SET FEATURE command */
-#define NAND_CMD_GET_FEATURE       ((uint8_t)0xEEu) /**< NAND GET FEATURE command */
-#define NAND_FEATURE_ARRAY_OP_MODE ((uint8_t)0x90u) /**< NAND Feature 'array operation mode' */
-#define NAND_FEATURE_ECC_ENABLE    ((uint8_t)0x08u) /**< NAND Feature 'array operation mode' ECC enable bit */
+#define NAND_TIMEOUT                    1000u          /**< NAND Timeout for ST HAL */
+#define NAND_MAX_BLOCK_SIZE_BYTES       262144u        /**< NAND block maximum size used for write buffering (correspond to 64 pages of 4096 bytes) */
+#define NAND_INVALID_BLOCK_NUMBER       ((uint32_t)-1) /**< NAND Invalid block id (used to default the value of nand_write_buffer_block) */
+#define NAND_CMD_SET_FEATURE            ((uint8_t)0xEFu) /**< NAND SET FEATURE command */
+#define NAND_CMD_GET_FEATURE            ((uint8_t)0xEEu) /**< NAND GET FEATURE command */
+#define NAND_FEATURE_ARRAY_OP_MODE      ((uint8_t)0x90u) /**< NAND Feature 'array operation mode' */
+#define NAND_FEATURE_ECC_ENABLE         ((uint8_t)0x08u) /**< NAND Feature 'array operation mode' ECC enable bit */
+#define NAND_CMD_GET_STATUS             ((uint8_t)0x70u) /**< NAND GET Status command */
+#define NAND_STATUS_ECC_UNCORRECTABLE   ((uint8_t)0x01u) /**< More than 8 bit errors detected */
+#define NAND_STATUS_ECC_CORRECTION_MASK ((uint8_t)0x18u) /**< ECC correction level mask, bits 4:3 */
+#define NAND_STATUS_ECC_NO_ERROR        ((uint8_t)0x00u) /**< No bit error detected */
+#define NAND_STATUS_ECC_1_TO_3          ((uint8_t)0x08u) /**< 1 to 3 bit errors corrected */
+#define NAND_STATUS_ECC_4_TO_6          ((uint8_t)0x10u) /**< 4 to 6 bit errors corrected */
+#define NAND_STATUS_ECC_7_TO_8          ((uint8_t)0x18u) /**< 7 to 8 bit errors corrected */
 
 /***************************** Types Definitions *****************************/
 
@@ -35,11 +42,24 @@
  */
 typedef struct
 {
-    uint8_t p1; /**< NAND feature parameters 1 */
-    uint8_t p2; /**< NAND feature parameters 2 */
-    uint8_t p3; /**< NAND feature parameters 3 */
-    uint8_t p4; /**< NAND feature parameters 4 */
+    uint8_t p1; /**< @brief NAND feature parameters 1 */
+    uint8_t p2; /**< @brief NAND feature parameters 2 */
+    uint8_t p3; /**< @brief NAND feature parameters 3 */
+    uint8_t p4; /**< @brief NAND feature parameters 4 */
 } nandFeatureData_t;
+
+/**
+ * @enum  nandEccStatus_t
+ * @brief   Enum type definition of NAND ECC status for a page
+ */
+typedef enum
+{
+    NAND_ECC_NO_ERROR,         /**< Page had no ECC error */
+    NAND_ECC_CORRECTED_1_TO_3, /**< Page had 1 to 3 bits error but it has been corrected */
+    NAND_ECC_CORRECTED_4_TO_6, /**< Page had 4 to 6 bits error but it has been corrected */
+    NAND_ECC_CORRECTED_7_TO_8, /**< Page had 7 to 8 bits error but it has been corrected */
+    NAND_ECC_UNCORRECTABLE     /**< Page had more than 8 bits error and can't be corrected */
+} nandEccStatus_t;
 
 /*************************** Functions Declarations **************************/
 
@@ -50,6 +70,8 @@ static returnCode_t NandSetupIOs(nandInst_t *nand_inst, const nandConf_t *const 
 static returnCode_t NandEnableECC(nandInst_t *nand_inst);
 static returnCode_t NandGetFeature(nandInst_t *nand_inst, uint8_t feature_address, nandFeatureData_t *feature_data);
 static returnCode_t NandSetFeature(nandInst_t *nand_inst, uint8_t feature_address, const nandFeatureData_t *feature_data);
+static returnCode_t NandReadPage(nandInst_t *nand_inst, memorySector_t sector, data_t data, nandEccStatus_t *ecc_status);
+static returnCode_t NandReadStatus(nandInst_t *nand_inst, uint8_t *status);
 
 /*************************** Variables Definitions ***************************/
 
@@ -288,11 +310,19 @@ returnCode_t NandRead(nandInst_t *nand_inst, memorySector_t sector, data_t data,
     // Check parameter(s)
     if ((nand_inst != NULL) && (length != 0u) && (data != NULL))
     {
-        NAND_AddressTypeDef nand_addr = NandLinearToAddress(nand_inst, sector);
-        HAL_StatusTypeDef test_hal    = HAL_NAND_Read_Page_8b(&nand_inst->handle_struct, &nand_addr, data, length);
-        if (test_hal != HAL_OK)
+        uint32_t page = 0u;
+        while ((page < length) && (return_value == RET_SUCCESSFUL))
         {
-            KernelPanic();
+            nandEccStatus_t ecc_status;
+            return_value = NandReadPage(nand_inst, sector + page, &data[page * nand_inst->p_conf->page_size], &ecc_status);
+            page++;
+
+            // Decode ECC status
+            if ((return_value == RET_SUCCESSFUL) &&
+                (ecc_status == NAND_ECC_UNCORRECTABLE))
+            {
+                return_value = RET_ERROR; // TO DO : implement a proper FTL
+            }
         }
     }
     else
@@ -811,6 +841,147 @@ static returnCode_t NandSetFeature(nandInst_t *nand_inst, uint8_t feature_addres
             {
                 hnand->State = HAL_NAND_STATE_ERROR;
             }
+        }
+        else if (hnand->State == HAL_NAND_STATE_BUSY)
+        {
+            return_value = RET_NOT_AVAILABLE;
+        }
+        else
+        {
+            return_value = RET_ERROR;
+        }
+    }
+    else
+    {
+        return_value = RET_INVALID_PARAM;
+    }
+
+    return return_value;
+}
+
+/**
+ * @fn              NandReadPage(nandInst_t *nand_inst, memorySector_t sector, data_t data, nandEccStatus_t *ecc_status)
+ * @brief           Reads one NAND page and reports its internal ECC status
+ * @param[in,out]   nand_inst   Instance that contains NAND handlers
+ * @param[in]       sector      Linear sector number of the page to read
+ * @param[out]      data        Buffer receiving one complete NAND page
+ * @param[out]      ecc_status  Internal ECC result associated with the page
+ * @retval          #RET_INVALID_PARAM if a parameter is invalid
+ * @retval          #RET_NOT_AVAILABLE if the NAND controller is busy
+ * @retval          #RET_TIMEOUT if the page read times out
+ * @retval          #RET_ERROR if the page or status read fails
+ * @retval          #RET_SUCCESSFUL if the page and its ECC status were read
+ */
+static returnCode_t NandReadPage(nandInst_t *nand_inst, memorySector_t sector, data_t data, nandEccStatus_t *ecc_status)
+{
+    returnCode_t return_value = RET_SUCCESSFUL;
+
+    // Check parameter(s)
+    if ((nand_inst != NULL) && (data != NULL) && (ecc_status != NULL))
+    {
+        // Get sector's NAND adress
+        NAND_AddressTypeDef nand_addr = NandLinearToAddress(nand_inst, sector);
+
+        // Read Page
+        HAL_StatusTypeDef test_hal = HAL_NAND_Read_Page_8b(&nand_inst->handle_struct, &nand_addr, data, 1u);
+        if (test_hal == HAL_OK)
+        {
+            uint8_t raw_status;
+
+            // Read Status
+            return_value = NandReadStatus(nand_inst, &raw_status);
+            if (return_value == RET_SUCCESSFUL)
+            {
+                if ((raw_status & NAND_STATUS_ECC_UNCORRECTABLE) != 0u)
+                {
+                    *ecc_status = NAND_ECC_UNCORRECTABLE;
+                }
+                else
+                {
+                    switch (raw_status & NAND_STATUS_ECC_CORRECTION_MASK)
+                    {
+                        case NAND_STATUS_ECC_NO_ERROR :
+                            *ecc_status = NAND_ECC_NO_ERROR;
+                            break;
+
+                        case NAND_STATUS_ECC_1_TO_3 :
+                            *ecc_status = NAND_ECC_CORRECTED_1_TO_3;
+                            break;
+
+                        case NAND_STATUS_ECC_4_TO_6 :
+                            *ecc_status = NAND_ECC_CORRECTED_4_TO_6;
+                            break;
+
+                        case NAND_STATUS_ECC_7_TO_8 :
+                            *ecc_status = NAND_ECC_CORRECTED_7_TO_8;
+                            break;
+
+                        default :
+                            return_value = RET_ERROR;
+                            break;
+                    }
+                }
+            }
+        }
+        else if (test_hal == HAL_BUSY)
+        {
+            return_value = RET_NOT_AVAILABLE;
+        }
+        else if (test_hal == HAL_TIMEOUT)
+        {
+            return_value = RET_TIMEOUT;
+        }
+        else
+        {
+            return_value = RET_ERROR;
+        }
+    }
+    else
+    {
+        return_value = RET_INVALID_PARAM;
+    }
+
+    return return_value;
+}
+
+/**
+ * @fn              NandReadStatus(nandInst_t *nand_inst, uint8_t *status)
+ * @brief           Read NAND status register
+ * @param[in,out]   nand_inst   Instance that contains NAND handlers
+ * @param[out]      status      Feature data that has been read
+ * @retval          #RET_INVALID_PARAM if nand_inst or status is a null pointer
+ * @retval          #RET_NOT_AVAILABLE if NAND is already in use
+ * @retval          #RET_TIMEOUT if the NAND times out during operation
+ * @retval          #RET_ERROR if the NAND controller is in an invalid state
+ * @retval          #RET_SUCCESSFUL else
+ */
+static returnCode_t NandReadStatus(nandInst_t *nand_inst, uint8_t *status)
+{
+    returnCode_t return_value = RET_SUCCESSFUL;
+
+    // Check parameter(s)
+    if ((nand_inst != NULL) && (status != NULL))
+    {
+        NAND_HandleTypeDef *hnand = &nand_inst->handle_struct;
+
+        if (hnand->State == HAL_NAND_STATE_READY)
+        {
+            // Update the NAND controller state
+            hnand->State = HAL_NAND_STATE_BUSY;
+
+            // Enter GET FEATURES mode
+            *(__IO uint8_t *)((uint32_t)(NAND_DEVICE | CMD_AREA)) = NAND_CMD_STATUS;
+            __DSB();
+
+            // Get Status
+            *status = *(__IO uint8_t *)NAND_DEVICE;
+
+            // Return to read mode
+            *(__IO uint8_t *)((uint32_t)(NAND_DEVICE | CMD_AREA)) = NAND_CMD_AREA_A;
+            __DSB();
+
+            // Reset NAND state
+            hnand->State = HAL_NAND_STATE_READY;
         }
         else if (hnand->State == HAL_NAND_STATE_BUSY)
         {
